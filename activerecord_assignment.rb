86a9ae1fb6bc41d1775f0ab7a4533150b14f47de
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-13 14:59:47 -0400

    Assignment complete

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -5,22 +5,23 @@
 
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
-
+  Genre.where("name=?", "Hip Hop/Rap")
 
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
-
+a=Track.where("genre_id=?", 17).count
 
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
-
+a=Track.sum("milliseconds")
 
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
-
+a=Track.where("media_type_id=?", 1).maximum("unit_price")
 
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
+a=Track.where("media_type_id=?", 1).order(:unit_price, created_at: :DESC).limit(1)
 
 
 
@@ -32,3 +33,4 @@
 
 
 # 1) Find all the albums whose titles start with B.
+a = Album.where("title LIKE ?",'B%')
